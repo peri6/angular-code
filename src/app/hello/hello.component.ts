9fb859fc9a5cf5f51392c8c3c551a1a1bcbafb76
prerna_dev <prerna.ghante@hdfcsec.com>
2023-08-16 10:46:15 +0530

    first commit

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-hello',
+    template: '<p>{{ message }}</p>'
+})
+export class HelloComponent implements OnInit {
+    message: string;
+
+    constructor(private http: HttpClient) {
+      this.message = '';
+    }
+
+    ngOnInit(): void {
+        this.http.get<{ message: string }>('http://127.0.0.1:5000/api/hello').subscribe(data => {
+            this.message = data.message;
+        });
+    }
+}
