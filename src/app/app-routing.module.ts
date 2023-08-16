e081519fe219f05869abe5070714ecb9124dc40a
prerna ghante <45898992+peri6@users.noreply.github.com>
2023-08-16 11:43:42 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [{component:hellocomponent, path:'/hello'}];
+import { HelloComponent } from './hello/hello.component';
+const routes: Routes = [{component:HelloComponent, path:'/hello'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
